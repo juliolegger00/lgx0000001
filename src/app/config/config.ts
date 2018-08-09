24275253fdaff86e6a160eca375625a319b2980b
julio cardona <julio@leggercolombia.com>
2018-08-08 22:12:38 -0500

    firebase

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,7 +1,7 @@
 export class CONFIG {
 
-    public static api_wp_json =   "http://leggercolombia.com/wordpress/wp-json/wp/v2/" ;
-    public static api_jwt_auth =   "http://leggercolombia.com/wordpress/wp-json/jwt-auth/v1/token" ;
+    public static api_wp_json =   "https://leggercolombia.com/wordpress/wp-json/wp/v2/" ;
+    public static api_jwt_auth =   "https://leggercolombia.com/wordpress/wp-json/jwt-auth/v1/token" ;
     public static api_info_texto_vivienda= CONFIG.api_wp_json+"info_texto_vivienda/";
     public static api_texto_cotizacion_persona=CONFIG.api_info_texto_vivienda+"113";
     public static api_lista_proyectos_vivienda= CONFIG.api_wp_json+"proyectos_vivienda/";
